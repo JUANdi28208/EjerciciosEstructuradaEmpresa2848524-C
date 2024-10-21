e54f47a533e2fa642837afd6fa9bf94d9e9f2a60
= <moramillanjuandiego@gmail.com>
2024-10-21 17:40:59 -0500

    Ejercicios de empresa estructurada y objetos terminados

---
+++ EmpresaEstructurada/EmpresaObjetos/src/clasesempresa/Empresa.java
@@ -0,0 +1,61 @@
+package clasesempresa;
+
+import java.util.ArrayList;
+import javax.swing.SingleSelectionModel;
+
+
+public class Empresa {
+    ArrayList<Empleado> empleados;
+
+    public Empresa() {
+        empleados = new ArrayList<Empleado>();
+    }
+
+    public void contratarEmpleado(Empleado e) {
+        empleados.add(e);
+    }
+
+    public int getTotalempleados() {
+        return empleados.size();
+    }
+
+    public void nombreSalario() {
+        for(Empleado e:empleados) {
+            System.out.println("Nombre: " + e.getNombre() + " Salario" + e.getSalario());
+        }
+        
+    }
+
+    public double getTotalSalarios() {
+        double total = 0;
+        for(Empleado e:empleados) {
+            total = total + e.getSalario();
+        }
+        return total;
+    }
+
+    public void EmpleadoMayorSalario() {
+        Empleado  EmpleadoMayorSalario = empleados.get(:0)
+        double mayorSalario =empleados.get(0).getSalario();
+        for(Empleado e:empleados) {
+            if(e.getSalario() > mayorSalario) {
+                mayorSalario = e.getSalario();
+                empMayorSalario = e;
+            }
+        }
+        System.out.println("Nombre: " + empMayorSalario.getNombre() + " Salario: " + empMayorSalario.getSalario());
+    }
+
+
+    public void EmpleadoMenorSalario() {
+        Empleado empMenorSalario = empleados.get(:0)
+        double menorSalario =empleados.get(0).getSalario();
+        for(Empleado e:empleados) {
+            if(e.getSalario() > menorSalario) {
+                menorSalario = e.getSalario();
+                empMenorSalario = e;
+            }
+        }
+        System.out.println("Nombre: " + empMenorSalario.getNombre() + " Salario: " + empMenorSalario.getSalario());
+    }
+}
